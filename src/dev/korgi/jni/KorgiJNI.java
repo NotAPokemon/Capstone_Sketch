ddce1a4e6f49642a6cd07819b985d7e9aef2553b
NotAPokemon <arvan.kadkol@gmail.com>
2025-12-27 22:57:16 -0800

    set up for cross-platform

--- src/dev/korgi/jni/KorgiJNI.java
+++ src/dev/korgi/jni/KorgiJNI.java
@@ -3,7 +3,16 @@ package dev.korgi.jni;
 public class KorgiJNI {
 
     static {
-        System.loadLibrary("korgigl");
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("mac")) {
+            System.loadLibrary("korgikompute-mac");
+        } else if (os.contains("win")) {
+            System.loadLibrary("korgikompute-win");
+        } else if (os.contains("nux")) {
+            System.loadLibrary("korgikompute-linux");
+        } else {
+            throw new UnsatisfiedLinkError("Unsupported OS I suggest you switch to mac to windows");
+        }
     }
 
     public static native void executeKernal(int[] pixels, int w, int h, float[] cam, float[] foward, float[] right,
